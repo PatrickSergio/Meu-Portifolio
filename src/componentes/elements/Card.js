de4b5e9ee1eff5cf0f9c16fcc9e8aed3e7ee2687
Patrick Sérgio <145050449+PatrickSergio@users.noreply.github.com>
2024-05-16 11:05:09 -0300

    Update Card.js

--- src/componentes/elements/Card.js
+++ src/componentes/elements/Card.js
@@ -1,22 +1,37 @@
 import styles from './Card.module.css'
 
 import ButtonB from './ButtonB'
+import {useState} from 'react'
 
 
 
+function Card({ title, tech, description, repo, img, site }) {
+    const [info, setInfo] = useState(false)
 
-function Card({title, tech, description, repo, img, site}) {
+    function infoOn() {
+        setInfo(true)
+    }
+
+    function infoOff() {
+        setInfo(false)
+    }
+
     return (
-        <div className={styles.card}>
-            <a href={site} target='blank'>
+        <div className={styles.card}  onMouseLeave={infoOff}>
+            <a onMouseEnter={infoOn} href={site} target='blank'>
                 <img src={img} alt='ERROR' /></a>
             
-            <section>
+            {info === true && (
+                  <section>
                 <h3>{title}</h3>
                 <p><strong>Tecnologia: </strong>{tech}</p>
                 <p>{description}</p>
                 <ButtonB text='Acesse o repositório' link={repo}></ButtonB>
             </section>
+                
+            )}
+
+          
             
 
 
