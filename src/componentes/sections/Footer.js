6c8c8aa7fc9877dedee1665f44654c00f611d736
Patrick Sérgio <sicck3@hotmail.com>
2024-05-27 20:42:47 -0300

    Atualizar o footer do portfólio

--- src/componentes/sections/Footer.js
+++ src/componentes/sections/Footer.js
@@ -9,11 +9,11 @@ function Footer() {
         <div className={styles.footer}>
             <ul>
                 <a href='https://www.instagram.com/_patricksergio/' target="_blank" rel='noreferrer'><li><FaInstagram size={50}></FaInstagram></li></a>
-                <li><FaLinkedin size={50}></FaLinkedin></li>
+                <a href='https://www.linkedin.com/in/patrick-moraes-477a7b308/'><li><FaLinkedin size={50}></FaLinkedin></li></a>
                 <a href='https://github.com/PatrickSergio' target="_blank" rel='noreferrer'><li><FaGithub size={50}></FaGithub></li></a>
             </ul>
             <p> Patrick Sérgio © 2024</p>
-            <p>sicck3@hotmail.com</p>
+            <a href='mailto:sicck3@hotmail.com'><p>sicck3@hotmail.com</p></a>
            
         </div>
         
